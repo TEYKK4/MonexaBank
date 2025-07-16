5c0cb87a11f09dcc0a41177f6c21a4d80e09b9fd
1 <teykasoc@gmail.com>
2025-07-17 01:45:19 +0200

    feat: add landing page

--- monexa-frontend/src/main.tsx
+++ monexa-frontend/src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {AuthProvider} from "@/context/AuthProvider.tsx";
+import { ThemeProvider } from './components/theme-provider.tsx'
+import {Toaster} from "sonner";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <AuthProvider>
-          <App />
-      </AuthProvider>
-  </StrictMode>,
+    <StrictMode>
+        <AuthProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                <App/>
+                <Toaster richColors position="top-right"/>
+            </ThemeProvider>
+        </AuthProvider>
+    </StrictMode>,
 )
