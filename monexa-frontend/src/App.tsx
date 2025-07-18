10ecfd4b623476fc772f733ba22e84221d82a2c4
1 <teykasoc@gmail.com>
2025-07-18 20:27:06 +0200

    feat: add dashboard route

--- monexa-frontend/src/App.tsx
+++ monexa-frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "@/pages/LoginPage.tsx";
 import RegisterPage from "@/pages/RegisterPage.tsx";
 import LandingPage from "@/pages/LandingPage.tsx";
+import Dashboard from "@/pages/Dashboard.tsx";
 function App() {
 
   return (
     <Router>
         <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
